45b36e51d63e1894790056bcd12302afdec1fcbf
Wonkee Kim <kimwonky@gmail.com>
2019-08-14 22:01:13 +1200

    on progress

---
+++ Assets/04_Scripts/CursorEventController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+
+
+public class CursorEventController : MonoBehaviour {
+
+
+    [SerializeField] private CursorObject cursorObject;
+    private Vector3 position;
+
+    #region Private Variables
+    public enum CursorStatus {
+        normal,
+        hover,
+        clicked
+    }
+    private CursorStatus currCursorStatus;
+
+    private Camera cam;
+
+
+    private bool isHovered = false;
+    #endregion
+
+
+    #region MonoBehaviour
+    private void Awake() {
+        if(cursorObject == null) {
+        }
+        cam = Camera.main;
+    }
+
+
+    private void Update() {
+
+
+    }
+    #endregion
+
+
+
+    #region Private Methods
+    private void MouseBehaviour() {
+        Vector2 mousePos = Input.mousePosition;
+        position = cam.ScreenToWorldPoint( new Vector3( mousePos.x, mousePos.y, cam.nearClipPlane ) );
+        cursorObject.Position = position;
+
+        if (Input.GetMouseButtonDown( 0 )) {
+            //ChangeStatus( CursorStatus.clicked );
+        }
+        if (Input.GetMouseButtonUp( 0 )) {
+            if (isHovered) {
+                //ChangeStatus( CursorStatus.hover );
+            } else {
+                //ChangeStatus( CursorStatus.normal );
+            }
+        }
+    }
+
+
+
+    private void ChangeStatus( CursorStatus _status ) {
+        if (currCursorStatus == _status) {
+            return;
+        } else {
+            currCursorStatus = _status;
+            switch (currCursorStatus) {
+                case CursorStatus.normal:
+                    //cursorSize_target = 0.3f;
+                    //mr.material.SetColor( shaderProp_color, Color.gray );
+                    break;
+                case CursorStatus.hover:
+                    //cursorSize_target = 0.3f;
+                    //mr.material.SetColor( shaderProp_color, Color.white );
+                    break;
+                case CursorStatus.clicked:
+                    //cursorSize_target = 0.8f;
+                    //mr.material.SetColor( shaderProp_color, Color.white );
+                    break;
+            }
+            //UpdateCursorStart();
+        }
+    }
+    #endregion
+}
---
+++ Assets/04_Scripts/CursorObject.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+public class CursorObject : MonoBehaviour {
+
+
+    public Vector3 Position {
+        set { this.transform.position = value; }
+    }
+
+    private MeshRenderer mr;
+    private int shaderProp_size;
+    private int shaderProp_color;
+
+    private float cursorSize_target = 0.3f;
+    private float cursorSize = 0.3f;
+    private float cursorSizeVelocity;
+    private bool isCursorSizeChanging = false;
+
+
+
+    #region MonoBehaviour
+    private void Awake() {
+        mr = this.GetComponent<MeshRenderer>();
+        shaderProp_size = Shader.PropertyToID( "_CursorSize" );
+        shaderProp_color = Shader.PropertyToID( "_MainColor" );
+    }
+
+    private void Start() {
+        UpdateCursorStart( 0.3f, Color.gray );
+    }
+
+    private void Update() {
+
+        if (isCursorSizeChanging) {
+            UpdateCursor();
+        }
+
+    }
+    #endregion
+
+
+
+    #region Private Methods
+
+
+
+
+    private void UpdateCursorStart( float? _target = null, Color? _color = null ) {
+        if (_target != null) {
+            cursorSize_target = (float)_target;
+        }
+        if (_color != null) {
+            mr.material.SetColor( shaderProp_color, (Color)_color );
+        }
+        isCursorSizeChanging = true;
+        timer_update = 0;
+    }
+
+    float timer_update = 0;
+    private void UpdateCursor() {
+        cursorSizeVelocity += ( cursorSize_target - cursorSize ) * Time.deltaTime * 5;
+        cursorSize += cursorSizeVelocity;
+        //if( Mathf.Abs(cursorSize_target - cursorSize) < 0.0001f ) {
+        //if ( cursorSizeVelocity < 0.01f ) {
+        //cursorSize = cursorSize_target;
+        //cursorSizeVelocity = 0.0f;
+        //isCursorSizeChanging = false;
+        //} else {
+        cursorSizeVelocity *= 0.9f;
+        //}
+        mr.material.SetFloat( shaderProp_size, cursorSize );
+
+        timer_update += Time.deltaTime;
+        if (timer_update > 1) {
+            timer_update = 0;
+            isCursorSizeChanging = false;
+        }
+    }
+    #endregion
+
+
+
+
+}
+
